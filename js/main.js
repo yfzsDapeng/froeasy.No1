21b72bc7b23ee857f1a4ecedf2d8756f28c80a9c
彭鹏 <yfzs_dapeng@163.com>
2017-07-26 14:07:30 +0800

    部分修改

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
-$(function(){
-    $(".reagent_nav_div").each(function(){
-        $(this).click(function(){
+$(function() {
+
+    // 导航栏滚动变色
+    var nav = $(".nav"); //得到导航对象
+    var win = $(window); //得到窗口对象
+    var sc = $(document);//得到document文档对象。
+    win.scroll(function () {
+        if (sc.scrollTop() >= 115) {
+            nav.css("background-color","rgba(181, 228, 232, 0.7)");
+        } else {
+            nav.css("background-color","whitesmoke");
+        }
+    });
+
+    //导航栏点击高亮
+
+
+// 试剂展示
+    $(".reagent_nav_div").each(function () {
+        $(this).click(function () {
             var v = $(this).attr("v");
-            $("#reagent_cover"+v).toggle()
+            $("#reagent_cover" + v).toggle()
+        })
+    });
+
+    $(".reagent_show_all").each(function () {
+        $(this).mouseenter(function () {
+            var w = $(this).attr("w");
+            $("#reagent_cover" + w).css({display: "block"})
+        })
+    });
+
+    $(".reagent_show_all").each(function () {
+        $(this).mouseleave(function () {
+            var w = $(this).attr("w");
+            $("#reagent_cover" + w).css({display: "none"})
         })
     });
 });
