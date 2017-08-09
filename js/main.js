77147cf7d22b783bc398fcaee6e1142adb1d4c30
彭鹏 <yfzs_dapeng@163.com>
2017-08-09 16:10:11 +0800

    大幅度修改

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(function() {
 
-    // 导航栏滚动变色
-    var nav = $(".nav"); //得到导航对象
-    var win = $(window); //得到窗口对象
-    var sc = $(document);//得到document文档对象。
-    win.scroll(function () {
-        if (sc.scrollTop() >= 80) {
-            nav.css("background-color","rgba(181, 228, 232, 0.7)");
-        } else {
-            nav.css("background-color","transparent");
-        }
-    });
+    // // 导航栏滚动变色
+    // var nav = $(".nav"); //得到导航对象
+    // var win = $(window); //得到窗口对象
+    // var sc = $(document);//得到document文档对象。
+    // win.scroll(function () {
+    //     if (sc.scrollTop() >= 80) {
+    //         nav.css("background-color","rgba(181, 228, 232, 0.7)");
+    //     } else {
+    //         nav.css("background-color","transparent");
+    //     }
+    // });
 
 // 试剂展示
     $(".reagent_nav_div").each(function () {
